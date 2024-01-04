26d51972997ffee39685c5a789106bc7e097a1d1
Carlos Molina <cmolina@redhat.com>
2024-01-04 12:11:19 -0500

    Replaced desktop to KDE Plasma Updated utilities for KDE Plasma

--- fedora_csb_deploy_rh.sh
+++ fedora_csb_deploy_rh.sh
@@ -15,6 +15,7 @@ add_repositories() {
   rpm --import https://packages.microsoft.com/keys/microsoft.asc
   sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
   echo "Adding Slack repository..."
+  rpm --import https://slack.com/gpg/slack_pubkey_20230710.gpg
   dnf copr enable -y jdoss/slack-repo
 }
 
@@ -42,19 +43,20 @@ echo "Improving DNF performance..."
 echo -e "#Improve DNF download speed and performance\nmax_parallel_downloads=10\nfastestmirror=True\ninstallonly_limit=2" >> /etc/dnf/dnf.conf
 echo "Running initial Fedora updates..."
 dnf update -y
-dnf install -y slack-repo
-echo "Enabling Gnome Experimental features"
-gsettings reset org.gnome.mutter experimental-features
 
 # Add repositories and run commands before package selection
 add_repositories
+dnf install --nogpgcheck -y slack-repo
 
 # Initial installation
 echo "Updating package repository and installing initial packages..."
 dnf update -y
 dnf install -y https://binaries.webex.com/WebexDesktop-CentOS-Official-Package/Webex.rpm https://zoom.us/client/5.16.2.8828/zoom_x86_64.rpm
 dnf update -y
-dnf install --best --allowerasing -y arj cabextract code @development-tools dnf-utils dpkg file-roller fprintd-devel gimp gimp-data-extras gimp-*-plugin gimp-elsamuko gimp-*-filter gimp-help gimp-help-es gimp-layer* gimp-lensfun gimp-*-masks gimp-resynthesizer gimp-save-for-web gimp-separate+ gimp-*-studio gimp-wavelet* gimpfx-foundry gitg gnome-screenshot go hexchat htop hunspell-es info innoextract lha libcurl-devel libfprint-devel libreoffice-draw libreoffice-langpack-es libreoffice-help-es libxml2-devel lzma mozilla-ublock-origin neofetch nodejs-bash-language-server perl pstoedit pycharm-community pycharm-community-doc pycharm-community-plugins redhat-lsb-core slack thunderbird unace unrar wireshark xkill
+dnf group install --best --allowerasing -y "KDE Plasma Workspaces" "KDE (K Desktop Environment)"
+systemctl disable gdm
+systemctl enable sddm
+dnf install --best --allowerasing -y arj cabextract code @development-tools dnf-utils dpkg dragon elisa-player fprintd-devel gimp gimp-data-extras gimp-*-plugin gimp-elsamuko gimp-*-filter gimp-help gimp-help-es gimp-layer* gimp-lensfun gimp-*-masks gimp-resynthesizer gimp-save-for-web gimp-separate+ gimp-*-studio gimp-wavelet* gimpfx-foundry gwenview go htop hunspell-es info innoextract kamoso kate kcalc kdiskmark kget kommit konversation ksystemlog lha libcurl-devel libfprint-devel libreoffice-draw libreoffice-langpack-es libreoffice-help-es libxml2-devel lzma mozilla-ublock-origin neofetch nodejs-bash-language-server okular perl pstoedit pycharm-community pycharm-community-doc pycharm-community-plugins redhat-lsb-core slack thunderbird unace unrar wireshark xkill
 
 # Check if the initial installation was successful
 if [ $? -eq 0 ]; then
